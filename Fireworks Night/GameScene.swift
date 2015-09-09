8a3b170ba89463daa3e293fe096509a302f3978e
engjasonp <engjasonp@gmail.com>
2015-09-09 02:45:36 -0700

    Added properties to GameScene class. Added background image to didMoveToView() and instantiated gameTimer. Added func createFirework.

--- Fireworks Night/GameScene.swift
+++ Fireworks Night/GameScene.swift
@@ -9,7 +9,68 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameTimer: NSTimer!
+    var fireworks = [SKNode]()
+    
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var score: Int = 0 {
+        didSet {
+            // your code here
+        }
+    }
+    
     override func didMoveToView(view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        addChild(background)
+        
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(6, target: self, selector: "launchFireworks", userInfo: nil, repeats: true)
+
+    }
+    
+    func createFirework(#xMovement: CGFloat, x: Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch RandomInt(min: 0, max: 2) {
+        case 0:
+            firework.color = UIColor.cyanColor()
+            firework.colorBlendFactor = 1
+            
+        case 1:
+            firework.color = UIColor.greenColor()
+            firework.colorBlendFactor = 1
+            
+        case 2:
+            firework.color = UIColor.redColor()
+            firework.colorBlendFactor = 1
+            
+        default:
+            break
+        }
+        
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 0, y: 0))
+        path.addLineToPoint(CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.followPath(path.CGPath, asOffset: true, orientToPath: true, speed: 200)
+        node.runAction(move)
+        
+        let particlePath = NSBundle.mainBundle().pathForResource("fuse", ofType: "sks")!
+        let emitter = NSKeyedUnarchiver.unarchiveObjectWithFile(particlePath) as! SKEmitterNode
+        emitter.position = CGPoint(x: 0, y: -22)
+        node.addChild(emitter)
+        
+        fireworks.append(node)
+        addChild(node)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
